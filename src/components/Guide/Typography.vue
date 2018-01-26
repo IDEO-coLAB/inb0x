565abbb9cb57b582d4e1de7b8c2f116167181064
Michael Gingras <mcg79@cornell.edu>
2018-01-26 15:33:02 -0500

    type

--- src/components/Guide/Typography.vue
+++ src/components/Guide/Typography.vue
@@ -6,28 +6,26 @@
   <div>
     <h1>Typography</h1>
     <p>
-      This is a list of font-weights, and their most common use cases within inb0x.
+      This is a list of font-weights, and their most common use cases within inb0x.<br>
+      note: these are not yet implemented in the sass file. I think it would be great to first consult jacob on this, since he knows all the font-weights etc.
     </p>
 
     <div class="container">
       <div class="columns">
-        <div class="column col-6">nav</div>
+        <div class="column col-6" style="font-size: 24pt;">nav</div>
         <div class="column col-3">24pt/bold</div>
         <div class="column col-3">nav</div>
 
-        <div class="column col-6">body</div>
+        <div class="column col-6" style="font-size: 18pt;">body</div>
         <div class="column col-3">20pt</div>
         <div class="column col-3">medium</div>
 
-        <div class="column col-6">body</div>
-        <div class="column col-3">24pt</div>
-        <div class="column col-3">bold</div>
 
-        <div class="column col-6">card-title</div>
-        <div class="column col-3">24pt</div>
-        <div class="column col-3">bold</div>
+        <div class="column col-6" style="font-size: 14pt;">button</div>
+        <div class="column col-3">14pt</div>
+        <div class="column col-3">buttons</div>
 
-        <div class="column col-6">subheader</div>
+        <div class="column col-6 caps" style="font-size: 11pt;">subheader</div>
         <div class="column col-3">11pt/regular/caps</div>
         <div class="column col-3">titles</div>
 
